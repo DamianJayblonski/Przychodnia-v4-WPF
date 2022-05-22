2dd7db5f91f5324f65aababf45c6eb05bea25819
DamianJayblonski <damian.jablonski@microsoft.wsei.edu.pl>
2022-05-22 21:34:05 +0200

    Dodaj pliki projektów.

---
+++ Przychodnia v4/Migrations/20220522192434_InitialCreate.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Przychodnia_v4.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pacjents",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Imie = table.Column<string>(type: "TEXT", nullable: false),
+                    Nazwisko = table.Column<string>(type: "TEXT", nullable: false),
+                    Adres = table.Column<string>(type: "TEXT", nullable: false),
+                    Pesel = table.Column<string>(type: "TEXT", nullable: false),
+                    Plec = table.Column<string>(type: "TEXT", nullable: false),
+                    DataUrodzenia = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacjents", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RodzajZabiegus",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nazwa = table.Column<string>(type: "TEXT", nullable: false),
+                    Numer = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RodzajZabiegus", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wypiss",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Rezultat = table.Column<string>(type: "TEXT", nullable: false),
+                    DataWypisu = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataLeczeniaOd = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataLeczeniaDo = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PacjentID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wypiss", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Wypiss_Pacjents_PacjentID",
+                        column: x => x.PacjentID,
+                        principalTable: "Pacjents",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rozpoznanies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nazwa = table.Column<string>(type: "TEXT", nullable: false),
+                    Data = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    RodzajZabieguID = table.Column<int>(type: "INTEGER", nullable: false),
+                    PacjentID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rozpoznanies", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Rozpoznanies_Pacjents_PacjentID",
+                        column: x => x.PacjentID,
+                        principalTable: "Pacjents",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rozpoznanies_RodzajZabiegus_RodzajZabieguID",
+                        column: x => x.RodzajZabieguID,
+                        principalTable: "RodzajZabiegus",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rozpoznanies_PacjentID",
+                table: "Rozpoznanies",
+                column: "PacjentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rozpoznanies_RodzajZabieguID",
+                table: "Rozpoznanies",
+                column: "RodzajZabieguID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wypiss_PacjentID",
+                table: "Wypiss",
+                column: "PacjentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rozpoznanies");
+
+            migrationBuilder.DropTable(
+                name: "Wypiss");
+
+            migrationBuilder.DropTable(
+                name: "RodzajZabiegus");
+
+            migrationBuilder.DropTable(
+                name: "Pacjents");
+        }
+    }
+}
---
+++ Przychodnia v4/PacjentContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przychodnia_v2
+{
+    public class PacjentContext : DbContext
+    {
+        public DbSet<Pacjent> Pacjents { get; set; }
+        public DbSet<Rozpoznanie> Rozpoznanies { get; set; }
+        public DbSet<Wypis> Wypiss { get; set; }
+        public DbSet<RodzajZabiegu> RodzajZabiegus { get; set; }
+
+        public string DbPath { get; }
+
+        public PacjentContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path,"Przychodnia\\Przychodnia01.db");
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            Console.WriteLine(DbPath);
+            // dbContextOptionsBuilder.UseSqlite("Data Source =pacjent04.db");
+            dbContextOptionsBuilder.UseSqlite($"Data Source ={DbPath}");
+        }
+
+    }
+    public class Pacjent
+    {
+        public int ID { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Adres { get; set; }
+        public string Pesel { get; set; }
+        public string Plec { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+        public ICollection<Rozpoznanie> Rozpoznanies { get; set; }
+        public ICollection<Wypis> Wypiss { get; set; }
+
+    }
+    public class Rozpoznanie
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public DateTime Data { get; set; }
+        public int RodzajZabieguID { get; set; }
+        public RodzajZabiegu RodzajZabiegu { get; set; }
+        public int PacjentID { get; set; }
+        public Pacjent Pacjent { get; set; }
+    }
+    public class Wypis
+    {
+        public int ID { get; set; }
+        public string Rezultat { get; set; }
+        public DateTime DataWypisu { get; set; }
+        public DateTime DataLeczeniaOd { get; set; }
+        public DateTime DataLeczeniaDo { get; set; }
+        public int PacjentID { get; set; }
+        public Pacjent Pacjent { get; set; }
+    }
+    public class RodzajZabiegu
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Numer { get; set; }
+        public ICollection<Rozpoznanie> Rozpoznanies { get; set; }
+
+    }
+}
+
